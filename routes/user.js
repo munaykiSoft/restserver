f00efdd6213f474b0cf6bad351db70f348e9c8af
miguel159 <ok.rm1011@gmail.com>
2021-04-29 20:52:05 -0500

    aqw

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+
+
+
+
+
+
+
+
+
+
+
+const router = Router();
+
+router.get('/', userGet);
+
+router.put('/:id', userPut);
+
+router.post('/', userPost);
+
+router.delete('/', userDelete);
+
+router.patch('/', userPatch);
+
+
+module.exports = router;
